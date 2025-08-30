4dce4b37493a3469853cf597ebbba0e1b87ca76a
Aa <1729677089@qq.com>
2025-08-30 22:50:14 +0800

    update ScheduleController

--- src/main/java/org/oooooomy/controller/ScheduleController.java
+++ src/main/java/org/oooooomy/controller/ScheduleController.java
@@ -1,10 +1,11 @@
 package org.oooooomy.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import jakarta.annotation.Resource;
-import org.oooooomy.service.ScheduleInventoryService;
+import org.oooooomy.model.entity.Schedule;
 import org.oooooomy.service.ScheduleService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.oooooomy.utils.R;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -22,7 +23,54 @@ public class ScheduleController {
     @Resource
     private ScheduleService scheduleService;
 
-    @Resource
-    private ScheduleInventoryService scheduleInventoryService;
+    /**
+     * 分页查询班次
+     *
+     * @return R<Page<Schedule>>
+     */
+    @GetMapping
+    public R<Page<Schedule>> getAllSchedules(
+            @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+            @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize) {
+        Page<Schedule> page = new Page<>(pageNum, pageSize);
+        Page<Schedule> result = scheduleService.page(page);
+        return R.ok(result);
+    }
+
+    /**
+     * 根据id查询班次
+     *
+     * @param id 查询的班次id
+     * @return R<Schedule>
+     */
+    @GetMapping("/{id}")
+    public R<Schedule> getScheduleById(@PathVariable int id) {
+        Schedule schedule = scheduleService.getById(id);
+        return R.ok(schedule);
+    }
+
+    /**
+     * 删除班次
+     *
+     * @param id 删除的班次id
+     * @return R<Void>
+     */
+    @DeleteMapping("/{id}")
+    public R<Void> deleteScheduleById(@PathVariable int id) {
+        scheduleService.removeById(id);
+        return R.ok();
+    }
+
+    /**
+     * 更新班次
+     *
+     * @param schedule 班次
+     * @return R<Schedule>
+     */
+    @PutMapping
+    public R<Schedule> updateScheduleById(@RequestBody Schedule schedule) {
+        scheduleService.updateById(schedule);
+        return R.ok(schedule);
+    }
 
 }
